2e3af252daedc9e00597cf0ce1d2bbeb466ba4fd
heisenstack <dinaniamine@gmail.com>
2026-01-31 17:59:27 +0100

    adding host

--- src/Config.java
+++ src/Config.java
@@ -5,10 +5,12 @@ import java.util.*;
 public class Config {
     
     private List<Integer> ports;
+    private String host;
 
     
     public Config() {
         this.ports = new ArrayList<>();
+        this.host = "localhost";
     }
     public List<Integer> getPorts() {
         return ports;
@@ -16,5 +18,11 @@ public class Config {
     public void setPorts(List<Integer> ports) {
         this.ports = ports;
     }
+    public String getHost() {
+        return host;
+    }
+    public void setHost(String host) {
+        this.host = host;
+        }
     
 }
