94535fd105ebe82da02110bbfad01be006e2b9a9
heisenstack <dinaniamine@gmail.com>
2026-01-31 18:16:27 +0100

    add simple route object

--- src/Config.java
+++ src/Config.java
@@ -3,28 +3,41 @@ package src;
 import java.util.*;
 
 public class Config {
-    
+
     private List<Integer> ports;
     private String host;
     private List<Route> routes;
 
-    
     public Config() {
         this.ports = new ArrayList<>();
         this.host = "localhost";
+        this.routes = new ArrayList<>();
     }
+
     public List<Integer> getPorts() {
         return ports;
     }
+
     public void setPorts(List<Integer> ports) {
         this.ports = ports;
     }
+
     public String getHost() {
         return host;
     }
+
     public void setHost(String host) {
         this.host = host;
     }
+
+    public List<Route> getRoutes() {
+        return routes;
+    }
+
+    public void setRoutes(List<Route> routes) {
+        this.routes = routes;
+    }
+
     public static class Route {
         private String path;
         private List<String> allowedMethods;
@@ -32,15 +45,18 @@ public class Config {
         public String getPath() {
             return path;
         }
+
         public void setPath(String path) {
             this.path = path;
         }
+
         public List<String> getAllowedMethods() {
             return allowedMethods;
         }
+
         public void setAllowedMethods(List<String> allowedMethods) {
             this.allowedMethods = allowedMethods;
         }
     }
-    
+
 }
