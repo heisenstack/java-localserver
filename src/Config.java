a43450dc2107618df8a07831f692b2a30ebded7b
heisenstack <dinaniamine@gmail.com>
2026-01-31 17:53:00 +0100

    config setup

---
+++ src/Config.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.*;
+
+public class Config {
+    
+    private String host;
+    private List<Integer> ports;
+
+    
+    public Config() {
+        this.ports = new ArrayList<>();
+    }
+    
+    
+}
---
+++ src/ConfigLoader.java
@@ -0,0 +1,15 @@
+package src;
+import java.nio.file.*;
+
+import java.io.ObjectInputFilter.Config;
+
+public class ConfigLoader {
+    
+    public static Config load(String configPath) throws Exception {
+                Config config = new Config();
+                return config;
+
+    }
+    
+    
+}
