ed7242db7a40eb99e43654ff6c1d0b05d1418840
heisenstack <dinaniamine@gmail.com>
2026-01-31 19:30:26 +0100

    json parser

---
+++ src/JsonParser.java
@@ -0,0 +1,11 @@
+package src;
+
+public class JsonParser {
+    private String json;
+    private int index;
+
+    public JsonParser(String json) {
+        this.json = json;
+        this.index = 0;
+    }
+}
