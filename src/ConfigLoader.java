f54b990236ee7784873c8f4f9a37ae4cd2124742
heisenstack <dinaniamine@gmail.com>
2026-02-01 16:01:22 +0100

    parsing completedgit add .

--- src/ConfigLoader.java
+++ src/ConfigLoader.java
@@ -1,10 +1,14 @@
 package src;
 
+import java.util.Map;
+
 public class ConfigLoader {
     public static Config load(String configPath) throws Exception {
         String content = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(configPath)));
         System.out.println("config: " + content);
-        
+        JsonParser parser = new JsonParser(content);
+        Map<String, Object> jsonMap = parser.parseObject();
+        System.out.println("Parsed config: " + jsonMap);
         Config config = new Config();
         return config;
     }
