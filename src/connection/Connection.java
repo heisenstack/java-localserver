fafceb6ddf40a5b23735b0dab53c5e9b511b5c2b
heisenstack <dinaniamine@gmail.com>
2026-02-04 14:04:17 +0100

    connection

--- src/connection/Connection.java
+++ src/connection/Connection.java
@@ -1,6 +1,21 @@
 package src.connection;
 
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import src.http.HttpResponse;
 
 public class Connection {
+    private static final int BUFFER_SIZE = 8192;
+    private static final long TIMEOUT_MS = 30000; // 30 seconds
+
+    public Connection(SocketChannel channel) {
+    }
+
+    public void read() throws Exception {
+    }
+
+    public void write() throws Exception {
+
+    }
 
 }
