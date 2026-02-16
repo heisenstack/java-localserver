e977e61486ef52fa277a6d25236f6593a10bcb71
alaidi <abdlekhaliklaidi@gmail.com>
2026-02-16 15:29:22 +0000

    config server

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
 package src;
 
 import java.io.File;
+import java.util.List;
 
 import src.Config.Route;
 
 public class Main {
+
     public static void main(String[] args) {
+
         try {
-            Config config = ConfigLoader.load("config.json");
-            for (Route r : config.getRoutes()) {
-              if (r.isCgi()) {
-                  config.setCgiRoot(new File(r.getRoot()).getAbsolutePath());
-                  break;
+
+            List<Config> configs =
+                    ConfigLoader.load("config.json");
+
+            for (Config config : configs) {
+
+                for (Route r : config.getRoutes()) {
+                    if (r.isCgi()) {
+                        config.setCgiRoot(
+                                new File(r.getRoot())
+                                        .getAbsolutePath());
+                        break;
+                    }
+                }
+
+                new Thread(() -> {
+                    try {
+                        new Server(config).start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).start();
             }
-    }
-            new Server(config).start();
+
         } catch (Exception e) {
             System.err.println("[FATAL] Server failed to start");
             e.printStackTrace();
